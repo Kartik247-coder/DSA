da0fbc124626fc0b711aea1ccbf4135632fe1880
Kartik247-coder <kartikbobde234@gmail.com>
2026-08-09 23:02:40 +0530

    Time: 0 ms (100%), Space: 42.6 MB (94.77%) - LeetHub

---
+++ 1328-break-a-palindrome/1328-break-a-palindrome.java
@@ -0,0 +1,19 @@
+class Solution {
+    public String breakPalindrome(String pal) {
+        int n = pal.length();
+
+        if (n == 1) return "";
+
+        char[] s = pal.toCharArray();
+
+        for (int i = 0; i < n / 2; i++) {
+            if (s[i] != 'a') {
+                s[i] = 'a';
+                return new String(s);
+            }
+        }
+
+        s[n - 1] = 'b';
+        return new String(s);
+    }
+}
