ca3077768e93ef303e57af8101eb7d6fa3e9d2bc
Kartik247-coder <kartikbobde234@gmail.com>
2026-08-09 23:25:15 +0530

    Time: 0 ms (100%), Space: 42.2 MB (43.69%) - LeetHub

---
+++ 0991-broken-calculator/0991-broken-calculator.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int brokenCalc(int start, int target) {
+        if(start==target) return 0;
+        int count=0;
+        while(target>start){
+            if(target%2==0){
+                target=target/2;
+            }
+            else{
+                target=target+1;
+            }
+            count++;
+        }
+        return count+(start-target);
+        
+    }
+}
